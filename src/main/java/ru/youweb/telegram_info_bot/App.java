1f366e45d9446d6b5e18ebb5df8dde942a665efa
Dezer <mr.vladkuz@gmail.com>
2016-09-27 18:22:22 +0300

    basic tbot

---
+++ src/main/java/ru/youweb/telegram_info_bot/App.java
@@ -0,0 +1,13 @@
+package ru.youweb.telegram_info_bot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
