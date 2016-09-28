ac7b0149caf0b6ecb8bc5f0958fddea2c873d51f
irasikhin <i.rasikhni@gmail.com>
2016-09-28 11:22:56 +0300

    Добавил .gitignore. Вынес версии в проперти pom.xml. Отформатировал код.

--- src/main/java/ru/youweb/telegram_info_bot/App.java
+++ src/main/java/ru/youweb/telegram_info_bot/App.java
@@ -2,12 +2,9 @@ package ru.youweb.telegram_info_bot;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
