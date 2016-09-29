3d68676ada959c7f28248fe19a2cbffc1df7ea3a
irasikhin <i.rasikhni@gmail.com>
2016-09-29 13:42:08 +0300

    Тест генерации кода.

--- src/main/java/ru/youweb/telegram_info_bot/App.java
+++ src/main/java/ru/youweb/telegram_info_bot/App.java
@@ -1,10 +1,14 @@
 package ru.youweb.telegram_info_bot;
 
+import ru.youweb.jdbc.QCurrency;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
+        QCurrency currency = QCurrency.currency;
+
         System.out.println("Hello World!");
     }
 }
